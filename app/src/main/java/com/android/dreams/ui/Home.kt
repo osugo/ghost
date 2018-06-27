af79d19db1e4218cad5263446355c8623379203f
alexkombo <alex.kombo@radioafricagroup.co.ke>
2018-06-27 13:21:54 +0300

    adds bottom menu bar and respective menu items

--- app/src/main/java/com/android/dreams/ui/Home.kt
+++ app/src/main/java/com/android/dreams/ui/Home.kt
@@ -1,7 +1,8 @@
-package com.android.dreams
+package com.android.dreams.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.android.dreams.R
 
 class Home : AppCompatActivity() {
 
